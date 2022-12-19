b4a94a45388d1dc45ba7a9e6bc07f0c713b2ab13
Syamsoul Azrien <syamsoulazrien.miat@gmail.com>
2022-12-19 09:14:10 +0800

    delete query - conditions should be nullable

--- src/WowDB.php
+++ src/WowDB.php
@@ -12,7 +12,8 @@ class WowDB {
     public $singleData;
     public $multiData=Array();
 
-    function __construct($host='', $db='', $user='', $pwd='') {
+    function __construct($host='', $db='', $user='', $pwd='')
+    {
         
         $db_conf = false;
         
@@ -35,31 +36,35 @@ class WowDB {
         $this->seldb=mysqli_select_db($this->conn,$db_conf["database"]);
     }
 
-    public function query($da){
-        $query=$da;//["query"];
+    public function query($da)
+    {
+        $query = $da; //["query"];
 
         $result=mysqli_query($this->conn,$query);
         if($result){
-            if(is_bool($result) && $result){
-                //echo "inserted";
-            }else{
-                $this->numRows=mysqli_num_rows($result);
-                if($this->numRows>0){
+            if(!is_bool($result)){
+                $this->numRows = mysqli_num_rows($result);
+                
+                if($this->numRows > 0){
                     $this->singleData=mysqli_fetch_array($result);
                 }
-                if($this->numRows>1){
+
+                if($this->numRows > 1){
                     array_push($this->multiData,$this->singleData);
                     while($data=mysqli_fetch_assoc($result)){
                         array_push($this->multiData,$data);
                     }
                 }
             }
+
             return true;
         }
+
         return false;
     }
 
-    public function select($table, $cond=null, $cols=null){
+    public function select($table, $cond=null, $cols=null)
+    {
         $select_cols_raw = "";
 
         if(!empty($cols)){
@@ -82,21 +87,26 @@ class WowDB {
         $result=mysqli_query($this->conn,$query);
         if($result){
             $this->numRows=mysqli_num_rows($result);
-            if($this->numRows>0){
+            
+            if($this->numRows > 0){
                 $this->singleData=mysqli_fetch_assoc($result);
                 array_push($this->multiData,$this->singleData);
             }
-            if($this->numRows>1){
+
+            if($this->numRows > 1){
                 while($data=mysqli_fetch_assoc($result)){
                     array_push($this->multiData,$data);
                 }
             }
+
             return true;
         }
+
         return false;
     }
 
-    public function insert($table, $params, $id_column='id'){
+    public function insert($table, $params, $id_column='id')
+    {
         $query="INSERT INTO $table ";
         $col="(";
         $data="VALUES (";
@@ -107,43 +117,51 @@ class WowDB {
             $data.="'$cdata'".(($i+1)<count($params)?",":") ");
             $i++;
         }
-        $query.=$col.$data;
+        $query .= $col.$data;
 
-        $result=mysqli_query($this->conn,$query);
+        $result=mysqli_query($this->conn, $query);
         if($result){
             if(is_bool($result) && $result){
                 $current_id = 'error: id_col_is_not_valid';
-                $indexColName=$id_column;
-                $indexResult=mysqli_query($this->conn,"SELECT $indexColName FROM $table ORDER BY $indexColName ASC");
+                $indexColName = $id_column;
+                $indexResult = mysqli_query($this->conn, "SELECT $indexColName FROM $table ORDER BY $indexColName ASC");
+                
                 if(!$indexResult) return true;
-                else while($cID=mysqli_fetch_array($indexResult)) $current_id=$cID[$indexColName];
+                else while($cID = mysqli_fetch_array($indexResult)) $current_id = $cID[$indexColName];
 
                 return $current_id;
             }
         }
+
         return false;
     }
 
-    public function delete($table,$cond){
+    public function delete($table, $cond=null)
+    {
         $query="DELETE FROM $table ";
-        $query.="WHERE ";
 
-        $i=0;
-        foreach($cond as $ccol=>$cdata){
-            $query.="$ccol='$cdata'".(($i+1)<count($cond)?" AND ":" ");
-            $i++;
+        if($cond !== null && is_array($cond)){
+            $query.="WHERE ";
+
+            $i=0;
+            foreach($cond as $ccol=>$cdata){
+                $query.="$ccol='$cdata'".(($i+1)<count($cond)?" AND ":" ");
+                $i++;
+            }
         }
 
-        $result=mysqli_query($this->conn,$query);
+        $result=mysqli_query($this->conn, trim($query));
         if($result){
             if(is_bool($result) && $result){
                 return true;
             }
         }
+
         return false;
     }
 
-    public function update($table,$cond,$params){
+    public function update($table, $cond, $params)
+    {
         //UPDATE `tablesatu` SET `Id`=[value-1],`uname`=[value-2],`upass`=[value-3] WHERE 1
         $query="UPDATE $table ";
 
@@ -164,11 +182,12 @@ class WowDB {
                 return true;
             }
         }
-        return false;
 
+        return false;
     }
 
-    public function set($var,$val){
+    public function set($var,$val)
+    {
         $this->$var=$val;
     }
 }
